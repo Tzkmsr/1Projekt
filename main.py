a6d1f221503ec162f0f7637c8feece28a7803800
Tzkmsr <tezkymaser@seznam.cz>
2024-07-16 11:37:13 +0200

    uprava funkce insert_data

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def create():
     conne.commit()
     conne.close()
 
-def insert_data():
+def insert_data(teacher_name, teacher_age, teacher_address):
     conne = psycopg2.connect(
         dbname="student",
         user='postgres',
@@ -34,13 +34,13 @@ def insert_data():
     )
 
     cur = conne.cursor()
-    cur.execute(
-        '''
+    query = '''
     INSERT INTO teacher(name, age, address)
-    VALUES ('McGonagalova', 50, 'Bradavice')
+    VALUES (%s, %s, %s)
     '''
-    )
-
+    
+    cur.execute(query, (teacher_name, teacher_age, teacher_address))
     conne.commit()
     conne.close()
 
+insert_data('Lockhart', 35, 'Bradavice')
