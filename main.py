d243bb3d538ec0b56ed3cc0d584a87972b2ea2ce
Tzkmsr <tezkymaser@seznam.cz>
2024-07-16 09:00:28 +0200

    napojeni main.py na psycopg2 + vytvorena table teachers

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import psycopg2
 
+def create():
+    conne = psycopg2.connect(
+        dbname="student",
+        user='postgres',
+        password='admin',
+        host='localhost',
+        port='5432'
+
+    )
+
+    cur = conne.cursor()
+    cur.execute(
+        '''CREATE TABLE teacher(
+            ID SERIAL,
+            NAME TEXT,
+            AGE INT,
+            ADRESS TEXT
+        )'''
+    )
+
+    conne.commit()
+    conne.close()
+
+    create()
